b54a79c644923d05e11966346c480ee1445d4997
meplengg <phichaya.jaim@bumail.net>
2026-05-07 00:23:34 +0700

    switch to pytorch model

--- app/model.py
+++ app/model.py
@@ -1,14 +1,21 @@
-import onnxruntime as ort
+import timm
+import torch
 import numpy as np
 
 class Model:
     def __init__(self):
-        self.session = ort.InferenceSession("models/mobilenetv3.onnx")
+        self.model = timm.create_model(
+            "mobilenetv3_large_100",
+            pretrained=True
+        )
+        self.model.eval()
 
     def predict(self, x):
-        input_name = self.session.get_inputs()[0].name
-        outputs = self.session.run(
-            None,
-            {input_name: x}
-        )
-        return int(np.argmax(outputs[0]))
+        x = torch.tensor(x)
+
+        with torch.no_grad():
+            outputs = self.model(x)
+
+        pred = outputs.argmax(dim=1).item()
+
+        return int(pred)
